3f9d8ec871ba51982a6ce036ab5210f32d35e906
jysaa5 <jysaa5@naver.com>
2020-11-16 00:16:37 +0900

    Study TypeScript

---
+++ Basic/app.ts
@@ -0,0 +1,9 @@
+/*타입스크립트로 각 변수 타입 설정하기*/
+// 일반 변수에 타입 선언
+const personName: string = "violet";
+const personAge: number = 15;
+
+// 배열 변수에 타입 선언
+const TempNumberArray: number[] = [10, 20, 30];
+const TempArray: [boolean, number] = [true, 10];
+
