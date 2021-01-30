09f9f143b1f202eb75430c4ee0d2e0b6db194c77
jysaa5 <jysaa5@naver.com>
2021-01-30 23:25:15 +0900

    Study TypeScript

---
+++ Do_It/Project_v1.0/Object_Type/Class.ts
@@ -0,0 +1,8 @@
+/* 클래스 */
+class Person1 {
+    name: string
+    age?: number
+}
+
+let jack: Person1 = new Person1();
+jack.name = 'Jack';
--- Do_It/Project_v1.0/Object_Type/Interface.ts
+++ Do_It/Project_v1.0/Object_Type/Interface.ts
@@ -1,9 +1,28 @@
-interface IPerson01 {
+interface IPerson1 {
     name: string
     age: number
 }
 
-// 선택 속성 구문 (Optional property)
-interface IPerson02 {   
-
+/* 선택 속성 구문 (Optional property) */
+interface IPerson2 {
+    name: string // 필수 속성
+    age: number // 필수 속성
+    etc?: boolean  // 선택 속성
 }
+
+let good1: IPerson2 = {name: 'Jack', age: 32}
+let good2: IPerson2 = {name: 'Jack', age: 32, etc: true }
+
+/* 익명 인터페이스 (Anonymous interface) */
+let ai: {
+    name: string
+    age: number
+    etc?: boolean
+} = {name: 'Jack', age: 32}
+
+/* 함수에 사용된 익명 인터페이스 */
+function printMe(me: {name: string, age: number, etc?: boolean}){
+    console.log(me.etc? `${me.name} ${me.age} ${me.etc}`: `${me.name} ${me.age}`)
+}
+
+printMe(ai)
