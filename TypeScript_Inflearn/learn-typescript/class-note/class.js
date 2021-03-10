5abaccf3702aa2902daf7bd8cafa0fc282665464
jysaa5 <jysaa5@naver.com>
2021-03-10 21:38:30 +0900

    Study TypeScript

--- TypeScript_Inflearn/learn-typescript/class-note/class.js
+++ TypeScript_Inflearn/learn-typescript/class-note/class.js
@@ -1,5 +1,4 @@
 // ES2015 (ES6)
-
 class Person {
     // 클래스 로직
 
@@ -10,6 +9,15 @@ class Person {
         this.age = age;
     }
 }
-
 var joo = new Person('주연', 17);
-console.log(joo);
+
+
+// 생성자 함수
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+var capt = new Person('주연', 100);
+
+// 클래스와 생성자 함수가 같다. (클래스는 생성자 함수를 사용함.) 자바스크립트는 프로토타입 기반이다.
