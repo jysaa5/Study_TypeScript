f3828f915a15ff6221bad71d2dae77c98b659989
jysaa5 <jysaa5@naver.com>
2021-03-22 23:19:27 +0900

    Update 12_type-compatibility.ts

--- TypeScript_Inflearn/learn-typescript/class-note/12_type-compatibility.ts
+++ TypeScript_Inflearn/learn-typescript/class-note/12_type-compatibility.ts
@@ -4,10 +4,29 @@ interface Developer {
   skill: string;
 }
 
-interface Person {
+// interface Person {
+//     name: string;
+//     skill: string;
+// }
+
+class Person {
   name: string;
+  skill: string;
 }
 
 var developer: Developer;
 var person: Person;
-developer = person;
+developer = new Person();
+
+// 1. interface, interface
+// 호환 불가능
+// developer = person;
+// 호환 가능
+// person = developer;
+
+// 2. inteface, class
+// class Person {
+//   name: string;
+// }
+// 호환 불가능
+// developer = new Person();
